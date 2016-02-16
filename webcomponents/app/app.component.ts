b123d889bea0a98d31c02a48002f0a57ec95797b
Alexander Casall <alexander.casall@gmail.com>
2016-02-16 17:15:48 +0100

    example of accessing view children

--- webcomponents/app/app.component.ts
+++ webcomponents/app/app.component.ts
@@ -1,5 +1,11 @@
 import {Component} from 'angular2/core';
 import {MyWebcomponent} from "./mywebcomponent.component";
+import {ContentChildren} from "angular2/core";
+import {QueryList} from "angular2/core";
+import {ContentChild} from "angular2/core";
+import {AfterViewInit} from "angular2/core";
+import {ViewChild} from "angular2/core";
+import {ViewChildren} from "angular2/core";
 @Component({
     selector: 'my-app',
     directives: [MyWebcomponent],
@@ -9,6 +15,27 @@ import {MyWebcomponent} from "./mywebcomponent.component";
         <div body>I render in body.</div>
      </my-component>`
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+    @ViewChildren(MyWebcomponent)
+    entries: QueryList<MyWebcomponent>;
+
+    @ViewChild(MyWebcomponent)
+    mycomponent: MyWebcomponent;
+
+
+    ngAfterViewInit():any {
+
+        console.log("APP INIT");
+
+
+        this.entries.toArray().forEach(subcomp =>{
+            console.log(subcomp);
+        });
+
+        console.log(this.mycomponent);
+
+        console.log(this.mycomponent == this.entries.toArray()[0]);
+    }
 
 }
